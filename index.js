922157e5535b51a61890864f83b7df8b0a86d5f7
Ryder <thefifthcamel@gmail.com>
2026-05-24 21:00:04 -0700

    Made progress on getting data from canvas

--- index.js
+++ index.js
@@ -5,10 +5,39 @@ const path = require('path');
  
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-  console.log(process.env.API_KEY);
 });
+
+async function getStudentCourses() {
+  //get all the data from canvas
+  const baseUrl = process.env.BASE_URL;
+  const token = process.env.API_KEY;
+
+  const url = baseUrl + '/api/v1/courses';
+
+  try {
+    const responce = await fetch(url,{headers : {"Authorization" : `Bearer ${token}`}});
+
+    if (!responce.ok) {
+      throw new Error(`Responce status : ${responce.status}`);
+    }
+
+    const result = await responce.json();
+    const ret = [];
+    for (const course of result) {
+      ret.push({ id: course.id, name: course.name });
+    }
+
+    return ret;
+  } catch (error) {
+    console.error(error.message);
+  }
+  
+}
+
 
 const port = 3000;
 app.listen(port, () => {
-   console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
+  getStudentCourses().then(console.log);
 });
+
